4684c42e6cfb035632e707967ddbff90414835e7
MohammadMahdi Javid <you@example.com>
2022-07-12 01:03:57 +0430

    pprint and packet-bytes counter

--- models/packet.py
+++ models/packet.py
@@ -7,7 +7,8 @@ import itertools
 
 
 class Packet():
-    def __init__(self, src, srcp, dst, dstp, protocol_type, timestamp, eth, buf):
+    def __init__(self, src, srcp, dst, dstp, protocol_type, timestamp, eth,
+                 buf, payload_size, flags):
         '''
         src: Source IP Address
         srcp: Source Port number
@@ -18,26 +19,29 @@ class Packet():
         timestamp: Time when the packet is captured
         ethernet: Ethernet of the frame
         buffer: Buffer of the frame
+        payload_size: Size of the payload: len(payload) or len(protocol.data)
+        flags: Flags of the frame if it doesn't contain only SYN, AKC
         '''
-        self.src = src
+        self.src_ip = src
         self.srcp = srcp
-        self.dst = dst
+        self.dst_ip = dst
         self.dstp = dstp
         self.type = protocol_type
         self.timestamp = timestamp
         self.ethernet = eth
         self.protocol = "UNKNOWN"
-        self.__five_tuple = frozenset((self.src,
+        self.__five_tuple = frozenset((self.src_ip,
                                        self.srcp,
-                                       self.dst,
+                                       self.dst_ip,
                                        self.dstp,
                                        self.type,)
                                       )
         self.buffer = buf
+        self.payload_size = payload_size
+        self.flags = flags
 
     def __eq__(self, __o: object):
-        return self.five_tuple == __o.five_tuple and \
-            self.timestamp == __o.timestamp
+        return self.buffer == __o.buffer
 
     @classmethod
     def extract_create_packet(cls, timestamp, buf):
@@ -47,17 +51,38 @@ class Packet():
         src = socket.inet_ntoa(ip.src)
         dst = socket.inet_ntoa(ip.dst)
         protocol = ip.data
+        flags = None
+        payload_size = None
+        if isinstance(protocol, dpkt.icmp.ICMP):
+            protocol = protocol.data.data.udp
+        elif isinstance(protocol, dpkt.tcp.TCP):
+            # convert the flags to a string, 2 --> SYN
+            flags = dpkt.tcp.tcp_flags_to_str(protocol.flags)
+            # ['SYN', 'ACK']
+            flags = set(flags.split(','))
+            # don't add packets with only SYN and ACK flags
+            if not flags.difference({'SYN', 'ACK'}):
+                return  # return None if the packet is only SYN and ACK
+        if hasattr(protocol, 'data'):
+            # if it has payloard what is it's size
+            payload_size = len(protocol.data)
         srcp = protocol.sport
         dstp = protocol.dport
         protocol_type = protocol.__class__.__name__
-        return cls(src=src, srcp=srcp, dst=dst, dstp=dstp, protocol_type=protocol_type, timestamp=timestamp, eth=eth, buf=buf)
+        if src == '10.42.0.196' and dst == '142.250.185.46' or \
+                dst == '10.42.0.196' and src == '142.250.185.46':
+            len(protocol.data)
+            dpkt.tcp.TCP
+        return cls(src=src, srcp=srcp, dst=dst, dstp=dstp,
+                   protocol_type=protocol_type, timestamp=timestamp, eth=eth,
+                   buf=buf, payload_size=payload_size, flags=flags)
 
     @property
     def five_tuple(self):
         return self.__five_tuple
 
     def __str__(self):
-        return f'{self.src}:{self.srcp} -> {self.dst}:{self.dstp} over {self.type}'
+        return f'{self.src_ip}:{self.srcp} -> {self.dst_ip}:{self.dstp} over {self.type}'
 
     def __repr__(self):
         return self.__str__()
--- models/stream.py
+++ models/stream.py
@@ -1,3 +1,11 @@
+import sys
+import os
+import dpkt
+import datetime
+import socket
+import itertools
+
+
 class Stream():
     streams = {}
     count = 0
@@ -9,25 +17,43 @@ class Stream():
         index: contains the index of the stream
         src_pkt: contains the first packet in the stream 
             --> can give us source and destination IP and timestamp
-        sbytes: sent bytes of the stream
+        sbytes: sent bytes of the stream: first packet payload size
         rbytes: received bytes of the stream
         spkts: sent packets of the stream
         rpkts: received packets of the stream
         '''
+        if packet is None:
+            raise Exception("Error: Stream constructor didn't get a packet")
         self.src_pkt = packet
-        self.__packets = [] if packet is None else [packet]
-        self.__five_tuple = None if packet is None else packet.five_tuple
+        self.__packets = [packet]
+        self.__five_tuple = packet.five_tuple
         Stream.streams[self.__five_tuple] = self
         self.index = Stream.count
         Stream.count = Stream.count + 1
-        self.sbytes = 0
+        self.sbytes = packet.payload_size  # first packet is always sent
         self.rbytes = 0
-        self.spkts = 0
+        self.spkts = 1  # first packet is always sent
         self.rpkts = 0
 
     @property
     def packets(self):
         return self.__packets.copy()
+
+    def add_packet_info(self, packet):
+        '''
+        add sbytes, rbytes, spkts, rpkts to the stream with comparing 
+        the packet with the first packet in the stream
+        '''
+        if packet.src_ip == self.src_pkt.src_ip:
+            self.sbytes += packet.payload_size
+            self.spkts += 1
+        elif packet.dst_ip == self.src_pkt.src_ip:
+            self.rbytes += packet.payload_size
+            self.rpkts += 1
+        else:
+            raise Exception(
+                f'Error: source packet of the Stream is {self.src_pkt} and packet is {packet}'
+            )
 
     def add_packet_to_stream(self, packet):
         '''
@@ -36,7 +62,7 @@ class Stream():
         if packet in self.__packets:  # don't add if it's already in the list
             print('Packet already in Stream')
             return
-        # if this is the first packet added set the five tuple for Stream
+        # if this is the first packet added then set the five tuple for Stream
         if not self.__five_tuple and not self.__packets:
             raise Exception("Constructor didn't get a packet")
             self.__five_tuple = packet.five_tuple
@@ -44,6 +70,7 @@ class Stream():
         # if the five tuple is the same and the packet is not in the list and the list is not empty
         elif self.__five_tuple == packet.five_tuple:
             self.__packets.append(packet)
+            self.add_packet_info(packet)
         # if the five tuple is not the same and the list is not empty why add the packet?
         else:
             raise Exception("Error: Five tuple is not the same")
@@ -53,11 +80,15 @@ class Stream():
         '''
         create a Stream if it doesn't exist and add the packet to it
         '''
+        if packet is None:  # guard statement
+            return
+        # creates new stream if it doesn't exist
+        # adds the packet to it when it's the first packet
         if packet.five_tuple not in cls.streams:
             stream = cls(packet=packet)
-        else:
+        else:  # stream exists
             stream = cls.streams[packet.five_tuple]
-        stream.add_packet_to_stream(packet)
+            stream.add_packet_to_stream(packet)  # add packet to the stream
 
     @property
     def five_tuple(self):
@@ -72,4 +103,16 @@ class Stream():
 
     @classmethod
     def pprint(cls):
-        pass
+        for five_tuple, stream in cls.streams.items():
+            last_pkt = stream.packets[-1]
+            print(f'### flow number {stream.index}')
+            print(
+                f'{stream.src_pkt.src_ip}, {stream.src_pkt.srcp} --> \
+                {stream.src_pkt.dst_ip}, {stream.src_pkt.dstp}: \
+                {stream.src_pkt.type}: {stream.src_pkt.protocol}; \
+                sent packetsL {stream.spkts}, received packets: {stream.rpkts}, \
+                sent bytes: {stream.sbytes}, received bytes: {stream.rbytes}, \
+                timestamp: ({datetime.datetime.fromtimestamp(stream.src_pkt.timestamp)}, \
+                {datetime.datetime.fromtimestamp(last_pkt.timestamp)}) \
+                \n'
+            )
