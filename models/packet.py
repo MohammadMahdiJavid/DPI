bb405d5983f93f34497cdcaedfac639f747cee22
MohammadMahdi Javid <you@example.com>
2022-07-12 17:55:03 +0430

    bug fix and code improvement

--- models/packet.py
+++ models/packet.py
@@ -48,48 +48,87 @@ class Packet():
     @classmethod
     def catch_debugger(cls, *args, **kwargs):
         # ips = ['10.42.0.196', '142.250.185.46']
-        ips = ['143.204.11.14', '10.42.0.184']
-        if kwargs['src'] in ips and kwargs['dst'] in ips:
+        # ips = ['143.204.11.14', '10.42.0.184']
+        ips = ['10.42.0.196', '10.42.0.1']
+        ports = [31776, 53]
+        # if kwargs['srcp'] == 31776 or kwargs['dstp'] == 31776:
+        #     pass
+        if kwargs['src'] in ips and kwargs['dst'] in ips and \
+                kwargs['srcp'] in ports and kwargs['dstp'] in ports:
             print(f'frame {cls.debugger_counter}')
             cls.debugger_counter = cls.debugger_counter + 1
 
     @classmethod
+    def parse_TCP(cls, protocol):
+        # convert the flags to a string, 2 --> SYN
+        flags = dpkt.tcp.tcp_flags_to_str(protocol.flags)
+        # ['SYN', 'ACK']
+        flags = set(flags.split(','))
+        # don't add packets with only SYN flags
+        # if not flags.difference({'SYN'}):
+        #     return  # return None if the packet is only SYN
+        # # has only ACK in the flags
+        # if len(flags) == 1 and 'ACK' in flags \
+        #    and not has_payload:  # ACK and payload is empty
+        #     return  # return None if the packet is only ACK and it doesn't have payload
+        return flags
+
+    @classmethod
+    def parse_UDP(cls):
+        pass
+
+    @classmethod
     def extract_create_packet(cls, timestamp, buf):
+        # ethernet, source ip, destination ip, protocol: ICMP, UDP, TCP
+        eth, src, dst, protocol, ip = cls.parse_base(buf)
+        flags = None
+        payload_size = None
+        # has data attribute and not empty
+        has_payload = hasattr(protocol, 'data') and bool(protocol.data)
+        if isinstance(protocol, dpkt.icmp.ICMP):
+            # get the payload of ICMP
+            protocol = cls.parse_ICMP(protocol)
+            return  # drop ICMP packets
+        elif isinstance(protocol, dpkt.tcp.TCP):
+            if not has_payload:
+                return  # return None if the packet doesn't have payload
+            flags = cls.parse_TCP(protocol)
+        elif isinstance(protocol, dpkt.udp.UDP):
+            cls.parse_UDP()
+        srcp = protocol.sport
+        dstp = protocol.dport
+        protocol_type = protocol.__class__.__name__
+        cls.catch_debugger(src=src, dst=dst, timestamp=timestamp,
+                           eth=eth, srcp=srcp, dstp=dstp)
+        if has_payload:
+            # if it has payload what is it's size (in bytes)
+            payload_size = len(protocol.data)
+        return cls(src=src, srcp=srcp, dst=dst, dstp=dstp,
+                   protocol_type=protocol_type, timestamp=timestamp, eth=eth,
+                   buf=buf, payload_size=payload_size, flags=flags)
+
+    @classmethod
+    def parse_ICMP(cls, protocol):
+        protocol = protocol.data.data.udp
+        return protocol
+
+    @classmethod
+    def parse_base(cls, buf):
+        '''
+        parse basic info that every packet has
+        eth: Ethernet of the Frame
+        ip: ip packet of the Ethernet
+        src: Source IP Address
+        dst: Destination IP Address
+        protocol: Protocol of the ip packet
+        '''
         # Unpack the Ethernet frame (mac src/dst, ethertype)
         eth = dpkt.ethernet.Ethernet(buf)
         ip = eth.data
         src = socket.inet_ntoa(ip.src)
         dst = socket.inet_ntoa(ip.dst)
         protocol = ip.data
-        flags = None
-        payload_size = None
-        cls.catch_debugger(src=src, dst=dst, timestamp=timestamp, eth=eth,)
-        has_payload = hasattr(protocol, 'data') and bool(protocol.data)
-        if isinstance(protocol, dpkt.icmp.ICMP):
-            protocol = protocol.data.data.udp
-        elif isinstance(protocol, dpkt.tcp.TCP):
-            # convert the flags to a string, 2 --> SYN
-            flags = dpkt.tcp.tcp_flags_to_str(protocol.flags)
-            # ['SYN', 'ACK']
-            flags = set(flags.split(','))
-            # don't add packets with only SYN flags
-            # if not flags.difference({'SYN'}):
-            #     return  # return None if the packet is only SYN
-            # # has only ACK in the flags
-            # if len(flags) == 1 and 'ACK' in flags \
-            #    and not has_payload:  # ACK and payload is empty
-            #     return  # return None if the packet is only ACK and it doesn't have payload
-            if not has_payload:
-                return  # return None if the packet doesn't have payload
-        if has_payload:
-            # if it has payload what is it's size (in bytes)
-            payload_size = len(protocol.data)
-        srcp = protocol.sport
-        dstp = protocol.dport
-        protocol_type = protocol.__class__.__name__
-        return cls(src=src, srcp=srcp, dst=dst, dstp=dstp,
-                   protocol_type=protocol_type, timestamp=timestamp, eth=eth,
-                   buf=buf, payload_size=payload_size, flags=flags)
+        return eth, src, dst, protocol, ip
 
     @property
     def five_tuple(self):
