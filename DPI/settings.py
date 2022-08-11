fe9c88cb5db55f09d78cf4af1a3dba9c929ac666
MohammadMahdi Javid <mahdijavid1380@yahoo.com>
2022-08-11 14:50:00 +0430

    resolving issues #1, layer 3 is not present -> drop, removing comments

--- DPI/settings.py
+++ DPI/settings.py
@@ -2,4 +2,13 @@
 this file is used to configure settings for the DPI
 '''
 
-DEBUG = True
+import os
+
+
+def is_debugdir_exists():
+    current_path = os.getcwd()
+    debug_path = os.path.join(current_path, 'debug')
+    return os.path.exists(debug_path)
+
+
+DEBUG = is_debugdir_exists()
